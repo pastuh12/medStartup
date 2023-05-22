091f3330c0614e440fda660ea3b203bea66c6472
Romario <satanvsjiisus@gmail.com>
2023-05-22 20:08:20 +0300

    Добавил экран login (пока меню и AppBar видно везде, позже исправлю)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
+
 //import 'package:myapp/page-1/login.dart';
 // import 'package:myapp/page-1/home.dart';
 // import 'package:myapp/page-1/zapisiNaPriem.dart';
@@ -12,86 +13,107 @@ import 'package:myapp/utils.dart';
 // import 'package:myapp/page-1/lichniyKabinet.dart';
 // import 'package:myapp/page-1/analisi.dart';
 // import 'package:myapp/page-1/contacts.dart';
-import 'package:myapp/pages/testContacts.dart';
+//import 'package:myapp/pages/testContacts.dart';
+import 'package:myapp/pages/login.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-	int _currentIndex = 0;
+  int _currentIndex = 0;
+  List<String> menuTitles = ['Домашняя', 'Личный кабинет', 'Записи', 'Анализы', 'Контакты'];
 
-	@override
-	Widget build(BuildContext context) {
-	return MaterialApp(
-		title: 'Flutter',
-		debugShowCheckedModeBanner: false,
-		scrollBehavior: MyCustomScrollBehavior(),
-		theme: ThemeData(
-		primarySwatch: Colors.blue,
-		),
-		home: Scaffold(
-			appBar: AppBar(
-				 shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))),
-				title: Text(
-					'Контакты',
-					style: SafeGoogleFont (
-						'Inter',
-						fontSize: 20,
-						fontWeight: FontWeight.w600,
-						color: Color(0xffffffff),
-					),
-				),
-				backgroundColor: Color.fromARGB(255, 34, 147, 250),
-			),
-		body: SingleChildScrollView(
-			child: Scene(),
-		),
-			bottomNavigationBar: ClipRRect(
-		borderRadius: BorderRadius.only(
-		topLeft: Radius.circular(10),
-		topRight: Radius.circular(10),
-		),
-		child:BottomNavigationBar(
-
-				fixedColor: Color.fromARGB(255, 0, 0, 0),
-				elevation: 0,
-
-				currentIndex: _currentIndex,
-				onTap: (index){
-					setState((){
-						_currentIndex = index;
-					});
-				},
-				items: const [
-					BottomNavigationBarItem(
-							label: 'домашняя', icon: Icon(Icons.home, color: Colors.black,),backgroundColor: Color.fromARGB(
-							255, 205, 226, 248),
-					),
-					BottomNavigationBarItem(
-						label: 'личный кабинет', icon: Icon(Icons.person, color: Colors.black,),backgroundColor: Color.fromARGB(
-							255, 205, 226, 248),
-					),
-					BottomNavigationBarItem(
-						label: 'записи', icon: Icon(Icons.calendar_month, color: Colors.black,),backgroundColor: Color.fromARGB(
-							255, 205, 226, 248),
-					),
-					BottomNavigationBarItem(
-						label: 'анализы', icon: Icon(Icons.equalizer, color: Colors.black,),backgroundColor: Color.fromARGB(
-							255, 205, 226, 248),
-					),
-					BottomNavigationBarItem(
-						label: 'контакты', icon: Icon(Icons.phone, color: Colors.black,),backgroundColor: Color.fromARGB(
-							255, 205, 226, 248),
-					),
-				],
-			),
-		),
-		),
-	);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MyCustomScrollBehavior(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))),
+          title: Text(
+            menuTitles[_currentIndex],
+            style: SafeGoogleFont(
+              'Inter',
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Color(0xffffffff),
+            ),
+          ),
+          backgroundColor: Color.fromARGB(255, 34, 147, 250),
+        ),
+        // body: SingleChildScrollView(
+        //   child: Scene(),
+        // ),
+        body: Scene(),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+          child: BottomNavigationBar(
+            fixedColor: Color.fromARGB(255, 0, 0, 0),
+            elevation: 0,
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                label: menuTitles[0],
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.black,
+                ),
+                backgroundColor: Color.fromARGB(255, 205, 226, 248),
+              ),
+              BottomNavigationBarItem(
+                label: menuTitles[1],
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ),
+                backgroundColor: Color.fromARGB(255, 205, 226, 248),
+              ),
+              BottomNavigationBarItem(
+                label: menuTitles[2],
+                icon: Icon(
+                  Icons.calendar_month,
+                  color: Colors.black,
+                ),
+                backgroundColor: Color.fromARGB(255, 205, 226, 248),
+              ),
+              BottomNavigationBarItem(
+                label: menuTitles[3],
+                icon: Icon(
+                  Icons.equalizer,
+                  color: Colors.black,
+                ),
+                backgroundColor: Color.fromARGB(255, 205, 226, 248),
+              ),
+              BottomNavigationBarItem(
+                label: menuTitles[4],
+                icon: Icon(
+                  Icons.phone,
+                  color: Colors.black,
+                ),
+                backgroundColor: Color.fromARGB(255, 205, 226, 248),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
---
+++ mobile/lib/pages/login.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';
+
+class Scene extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+              //color: Colors.red,
+              padding: EdgeInsets.all(30),
+              child: Center(
+                child: Image.asset(
+                  'assets/page-1/images/icons8-health-64-1.png',
+                  //fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 205, 226, 248),
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(40))),
+              //color: Colors.amber,
+              padding: EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Вход',
+                    style: SafeGoogleFont(
+                      'Inter',
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    height: 51,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 18,
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 3),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Введите логин',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 51,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 18,
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 3),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Введите пароль',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 51,
+                    margin: EdgeInsets.symmetric(vertical: 17),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                      child: Text(
+                        'Вход',
+                        style: SafeGoogleFont(
+                          'Inter',
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ],
+              ),
+                  SizedBox(height: 20,width: 20,)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
