128300a45f6843fdf4694b9fd45478fefbd71017
Hip S <seryivol@gmail.com>
2023-05-24 01:48:37 +0300

    fvgh

---
+++ server/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ContactsController;
+use App\Http\Controllers\API\SpecializationsController;
+use App\Http\Controllers\API\DoctorsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::controller(RegisterController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('contacts', ContactsController::class);
+    Route::resource('specializations', SpecializationsController::class);
+});
+Route::resource('contacts2', ContactsController::class);
+Route::get('doctors', [DoctorsController::class, 'getAll']);
+
+///slkvnjcslkvslkvjslkvfsajlkvksdv
