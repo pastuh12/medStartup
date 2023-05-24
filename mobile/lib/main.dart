1fb7bd87691b0d2ea1cc408b5d4a9f7f41346239
Hip S <seryivol@gmail.com>
2023-05-24 23:20:23 +0300

    herrr

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
-import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/pages/async.dart';
 import 'package:myapp/utils.dart';
@@ -14,12 +13,12 @@ import 'package:myapp/utils.dart';
 // import 'package:myapp/page-1/lichniyKabinet.dart';
 // import 'package:myapp/page-1/analisi.dart';
 // import 'package:myapp/page-1/contacts.dart';
-//import 'package:myapp/pages/testContacts.dart';
+import 'package:myapp/pages/testContacts.dart';
 //import 'package:myapp/pages/login.dart';
 //import 'package:myapp/pages/analizy.dart';
 //import 'package:myapp/pages/zapisy.dart';
 //import 'package:myapp/pages/vibor.dart';
-import 'package:myapp/pages/vibor2.dart';
+//import 'package:myapp/pages/vibor2.dart';
 
 
 void main() => runApp(MyApp());
@@ -126,75 +125,5 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-
-
-	// @override
-	// Widget build(BuildContext context) {
-	// return MaterialApp(
-	// 	title: 'Flutter',
-	// 	debugShowCheckedModeBanner: false,
-	// 	scrollBehavior: MyCustomScrollBehavior(),
-	// 	theme: ThemeData(
-	// 	primarySwatch: Colors.blue,
-	// 	),
-	// 	home: Scaffold(
-	// 		appBar: AppBar(
-	// 			 shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))),
-	// 			title: Text(
-	// 				'Контакты',
-	// 				style: SafeGoogleFont (
-	// 					'Inter',
-	// 					fontSize: 20,
-	// 					fontWeight: FontWeight.w600,
-	// 					color: Color(0xffffffff),
-	// 				),
-	// 			),
-	// 			backgroundColor: Color.fromARGB(255, 34, 147, 250),
-	// 		),
-	// 	body: SingleChildScrollView(
-	// 		child: Scene(),
-	// 	),
-	// 		bottomNavigationBar: ClipRRect(
-	// 	borderRadius: BorderRadius.only(
-	// 	topLeft: Radius.circular(10),
-	// 	topRight: Radius.circular(10),
-	// 	),
-	// 	child:BottomNavigationBar(
-  //
-	// 			fixedColor: Color.fromARGB(255, 0, 0, 0),
-	// 			elevation: 0,
-  //
-	// 			currentIndex: _currentIndex,
-	// 			onTap: (index){
-	// 				setState((){
-	// 					_currentIndex = index;
-	// 				});
-	// 			},
-	// 			items: const [
-	// 				BottomNavigationBarItem(
-	// 						label: 'домашняя', icon: Icon(Icons.home, color: Colors.black,),backgroundColor: Color.fromARGB(
-	// 						255, 205, 226, 248),
-	// 				),
-	// 				BottomNavigationBarItem(
-	// 					label: 'личный кабинет', icon: Icon(Icons.person, color: Colors.black,),backgroundColor: Color.fromARGB(
-	// 						255, 205, 226, 248),
-	// 				),
-	// 				BottomNavigationBarItem(
-	// 					label: 'записи', icon: Icon(Icons.calendar_month, color: Colors.black,),backgroundColor: Color.fromARGB(
-	// 						255, 205, 226, 248),
-	// 				),
-	// 				BottomNavigationBarItem(
-	// 					label: 'анализы', icon: Icon(Icons.equalizer, color: Colors.black,),backgroundColor: Color.fromARGB(
-	// 						255, 205, 226, 248),
-	// 				),
-	// 				BottomNavigationBarItem(
-	// 					label: 'контакты', icon: Icon(Icons.phone, color: Colors.black,),backgroundColor: Color.fromARGB(
-	// 						255, 205, 226, 248),
-	// 				),
-	// 			],
-	// 		),
-	// 	),
-	// 	),
-	// );
-	// }
 }
+
