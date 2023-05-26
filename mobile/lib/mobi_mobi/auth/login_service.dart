90050b7c15e8741705871fb5fbb58bfa928157b7
Hip S <seryivol@gmail.com>
2023-05-27 02:59:24 +0300

    получили данные с бэка

---
+++ mobile/lib/mobi_mobi/auth/login_service.dart
@@ -0,0 +1,53 @@
+// import 'package:dio/dio.dart';
+import 'package:myapp/mobi_mobi/auth/user_model.dart';
+import 'package:myapp/mobi_mobi/const_only.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'login_interface.dart';
+
+class LoginService extends ILogin {
+  @override
+  Future<UserModel?> login(String email, String password) async {
+    final api = Uri.parse(urlServer);
+    final data = {"email": email, "password": password};
+    // final dio = Dio();
+    http.Response response;
+    response = await http.post(api, body: data);
+    if (response.statusCode == 200) {
+      SharedPreferences storage = await SharedPreferences.getInstance();
+      final body = json.decode(response.body);
+      await storage.setString('TOKEN', body['token']);
+      await storage.setString('EMAIL', email);
+      return UserModel(email: email, token: body['token']);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<UserModel?> getUser() async {
+    SharedPreferences storage = await SharedPreferences.getInstance();
+    final token = storage.getString('TOKEN');
+    final email = storage.getString('EMAIL');
+    if (token != null && email != null) {
+      return UserModel(email: email, token: token);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<bool> logout() async {
+    SharedPreferences storage = await SharedPreferences.getInstance();
+    final email = storage.getString('EMAIL');
+    final token = storage.getString('TOKEN');
+    if (email != null && token != null) {
+      await storage.remove('TOKEN');
+      await storage.remove('EMAIL');
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
